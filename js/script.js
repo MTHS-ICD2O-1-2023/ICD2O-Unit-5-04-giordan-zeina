b8179144ca59ed4bb3a77151d64c0774670f89ea
Giordan Zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-05-10 18:19:07 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Giordan Zeina All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: April 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  // input
+  const age = parseInt(document.getElementById('age-entered').value)
+  const dayTuesdayChecked = document.getElementById('option-tuesday').checked
+  const dayThursdayChecked = document.getElementById('option-thursday').checked
+
+  // process
+  if ((dayTuesdayChecked == true || dayThursdayChecked == true) || (age > 12 && age < 21)) {
+    document.getElementById('answer').innerHTML = "You get the student discount."
+  } else {
+    document.getElementById('answer').innerHTML = "You must pay regular price."
+  }
+}
